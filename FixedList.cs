55ee8a2065f99d0d8643be6ba3ad191b495ac179
Grebenuk Denis <32875166+yCatDev@users.noreply.github.com>
2020-04-01 11:06:54 +0300

    FixedList

---
+++ FixedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace YetAnotherSnake
+{
+    public class FixedList<T>
+    {
+        private T[] _array;
+        private int _counter;
+
+        public T this[int index]
+        {
+            get
+            {
+                 if (index>=_counter)
+                     throw new IndexOutOfRangeException();
+                 return _array[index];
+            }
+        }
+        
+        public FixedList(int maxSize)
+        {
+            _array = new T[maxSize];
+            _counter = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (_counter>=_array.Length)
+                throw new Exception("Fixed list is full");
+            _array[_counter] = item;
+            _counter++;
+        }
+
+        public int Length => _counter;
+
+    }
+}
